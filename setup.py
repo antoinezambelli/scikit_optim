7959d6b4afd64b2b3d5602a651ff67aeec87ab81
Antoine Emil Zambelli <antoine.zambelli@gmail.com>
2018-09-24 17:07:58 -0700

    init setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+#
+# dataengine setup.py
+#
+# Copyright(c) 2018, Carium, Inc. All rights reserved.
+#
+"""
+
+from setuptools import setup
+
+
+version = '1.0.0'
+
+setup(
+    name='scikit_optim',
+    version=version,
+    url='https://github.com/antoinezambelli/scikit_optim',
+    license='MIT',
+    author='Antoine Zambelli',
+    author_email='antoine.zambelli@gmail.com',
+    description='Scikit CV',
+    long_description='Scikit CV Convenience Library',
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy'
+    ]
+)

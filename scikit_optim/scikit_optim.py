4e46ea505f3469c3c73f3086de53fe8f2faebf9d
Antoine <antoine.zambelli@gmail.com>
2019-11-08 18:10:16 -0800

    dectree more granular

--- scikit_optim/scikit_optim.py
+++ scikit_optim/scikit_optim.py
@@ -570,7 +570,7 @@ class DecTree():
         else:
             parameters = {
                 'min_samples_split': np.unique(np.round(np.geomspace(2, min(len(X)/100.0, 25), num=10))).astype(int),
-                'max_features': ['auto', None]
+                'max_features': np.unique(np.round(np.geomspace(np.sqrt(X_in.shape[1]), X_in.shape[1], num=5))).astype(int)
             }
             dc = DecisionTreeClassifier()
             clf = RandomizedSearchCV(
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup, find_packages
 
 
-version = '3.2.0'
+version = '3.2.1'
 
 setup(
     name='scikit_optim',
